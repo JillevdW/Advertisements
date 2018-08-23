13e35d899c9487aa6da5519cf14663d7b405cef4
Jille <jille.van.der.weerd@beeproger.com>
2018-08-23 11:58:08 +0200

    Google admob plugin

--- app/item/items.component.ts
+++ app/item/items.component.ts
@@ -3,6 +3,10 @@ import { Component, OnInit } from "@angular/core";
 import { Item } from "./item";
 import { ItemService } from "./item.service";
 
+import * as Admob from "nativescript-admob";
+import { Page } from "tns-core-modules/ui/page/page";
+import { View, getViewById } from "tns-core-modules/ui/core/view";
+
 @Component({
     selector: "ns-items",
     moduleId: module.id,
@@ -11,11 +15,89 @@ import { ItemService } from "./item.service";
 export class ItemsComponent implements OnInit {
     items: Item[];
 
-    // This pattern makes use of Angular’s dependency injection implementation to inject an instance of the ItemService service into this class. 
-    // Angular knows about this service because it is included in your app’s main NgModule, defined in app.module.ts.
-    constructor(private itemService: ItemService) { }
+    private androidBannerId: string = "ca-app-pub-XXXX/YYYY";
+    private androidInterstitialId: string = "ca-app-pub-KKKK/LLLL";
+    private iosBannerId: string = "ca-app-pub-3940256099942544/2934735716";
+    // private iosBannerId: string = "/7191/ad/ad.mobile-ios";
+    // private iosInterstitialId: string = "ca-app-pub-GGGG/HHHH";
+
+    constructor(private itemService: ItemService, private page: Page) { }
 
     ngOnInit(): void {
         this.items = this.itemService.getItems();
     }
+
+    ngAfterViewInit(): void {
+        setTimeout(() => {
+            // this.createSmartBanner();
+            const view = this.page.getViewById("banner").nativeView;
+            // console.dir(view);
+            this.createBannerForView(view, Admob.AD_SIZE.SMART_BANNER);
+        }, 1000);
+    }
+
+    public createSmartBanner() {
+        this.createBanner(Admob.AD_SIZE.SMART_BANNER);
+    }
+
+    public createRegularBanner() {
+        this.createBanner(Admob.AD_SIZE.BANNER);
+    }
+
+    public createBannerForView(view: any, size: Admob.AD_SIZE) {
+        Admob.createBanner({
+            testing: true,
+            view: view,
+            size: size,
+            iosBannerId: this.iosBannerId,
+            androidBannerId: this.androidBannerId,
+            // iosTestDeviceIds: ["yourTestDeviceUDIDs"],
+            margins: {
+                bottom: 0
+            }
+        }).then(function() {
+            console.log("admob createBanner done");
+        }, function(error) {
+            console.log("admob createBanner error: " + error);
+        });
+    }
+
+    public createBanner(size: Admob.AD_SIZE) {
+        Admob.createBanner({
+            testing: true,
+            // view: view,
+            size: size,
+            iosBannerId: this.iosBannerId,
+            androidBannerId: this.androidBannerId,
+            iosTestDeviceIds: ["9328dfc3-20e1-4de1-bdd7-af77e389af8"],
+            margins: {
+                bottom: 200
+            }
+        }).then(function() {
+            console.log("admob createBanner done");
+        }, function(error) {
+            console.log("admob createBanner error: " + error);
+        });
+    }
+
+    public hideBanner() {
+        Admob.hideBanner().then(function() {
+            console.log("admob hideBanner done");
+        }, function(error) {
+            console.log("admob hideBanner error: " + error);
+        });
+    }
+
+    // public createInterstitial() {
+    //     Admob.createInterstitial({
+    //         testing: true,
+    //         iosInterstitialId: this.iosInterstitialId,
+    //         androidInterstitialId: this.androidInterstitialId,
+    //         iosTestDeviceIds: ["yourTestDeviceUDIDs"]
+    //     }).then(function() {
+    //         console.log("admob createInterstitial done");
+    //     }, function(error) {
+    //         console.log("admob createInterstitial error: " + error);
+    //     });
+    // }
 }
